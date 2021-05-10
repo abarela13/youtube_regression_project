109e21cf9cccd45a8f6198bacf1f468367efe9e0
Aaron <abarela13@gmail.com>
2021-05-10 15:58:22 -0700

    generate channels list

--- python/channelcrawler.py
+++ python/channelcrawler.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from os import path
+from os import path, listdir
 import requests
 import pickle
 import time
@@ -46,4 +46,30 @@ def read_pickle(file):
     with open(file, "rb") as f:
         return pickle.load(f)
 
-retrieve_results(1677375, 50)
+def channels_list():
+    download_directory = 'download/channelcrawler/'
+
+    result = []
+
+    for file in listdir(download_directory):
+        f = path.join(download_directory, file)
+
+        if path.isfile(f):
+            try:
+                 result.extend(list(read_pickle(f).keys()))
+            except:
+                raise Exception('Filetype was invalid')
+
+    
+    # Save page dictionary object
+    with open('download/channelcrawler/merged', "wb") as f:
+        pickle.dump(result, f)
+    
+    return result
+
+# print(list(read_pickle('download/channelcrawler/1677375_01').keys()))
+
+print(channels_list())
+
+
+# retrieve_results(1677375, 50)
