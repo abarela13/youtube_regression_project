e79e7cb3e3349c8dd85fb7407e08c4625e7c016f
Aaron <abarela13@gmail.com>
2021-05-10 17:09:49 -0700

    added more keys to channels dictionary

--- python/channelcrawler.py
+++ python/channelcrawler.py
@@ -1,75 +1,40 @@
 from bs4 import BeautifulSoup
-from os import path, listdir
+from math import ceil
 import requests
 import pickle
-import time
 
-def generate_dict(url, file_name):
+def retrieve_channels(url):
+    with open('data/channels_dict', "rb") as f:
+        channels_dict = pickle.load(f)
+
     data = requests.get(url)
 
     # load data into bs4
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    channel_dictionary = {}
-
     content = soup.findAll('div', {'class': 'row'})[1]
 
     for channel in content.find_all('div', {'class': 'channel'}):
-        title = channel.find('h4').find('a')['title']
-        id = channel.find('h4').find('a')['href'][31:]
-
-        channel_dictionary[id] = title
+        if channel not in channels_dict.keys():
+            id = channel.find('h4').find('a')['href'][31:]
+            channels_dict[id] = 'pending'
 
     # Save page dictionary object
-    with open(file_name, "wb") as f:
-        pickle.dump(channel_dictionary, f)
+    with open('data/channels_dict', "wb") as f:
+        pickle.dump(channels_dict, f)
 
-def retrieve_results(results, pages):
+def retrieve_results(result):
     # define base url
-    url = f'https://channelcrawler.com/eng/results/{results}/page:'
+    url = f'https://channelcrawler.com/eng/results/{result}/page:1'
+    data = requests.get(url)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    pages = ceil(int(soup.find('h3', {'class': 'results-title'}).text.strip().split(' ')[0]) / 20)
     
-    # define downloads folder directory
-    file_root = f'download/channelcrawler/{results}'
-
     for page in range(1, pages+1):
-        # generate file name
-        file_name = f'{file_root}_{str(page).zfill(2)}'
+        retrieve_channels(f'https://channelcrawler.com/eng/results/{result}/page:{page}')
+        print(f'page {page} completed')
 
-        if not path.isfile(file_name):
-            generate_dict(f'{url}{page}', file_name)
+results_list = [1688649, 1688651, 1688655, 1688656]
 
-            #Wait for page to load
-            time.sleep(20)
-
-def read_pickle(file):
-    # Read the soup object from a file
-    with open(file, "rb") as f:
-        return pickle.load(f)
-
-def channels_list():
-    download_directory = 'download/channelcrawler/'
-
-    result = []
-
-    for file in listdir(download_directory):
-        f = path.join(download_directory, file)
-
-        if path.isfile(f):
-            try:
-                 result.extend(list(read_pickle(f).keys()))
-            except:
-                raise Exception('Filetype was invalid')
-
-    
-    # Save page dictionary object
-    with open('download/channelcrawler/merged', "wb") as f:
-        pickle.dump(result, f)
-    
-    return result
-
-# print(list(read_pickle('download/channelcrawler/1677375_01').keys()))
-
-print(channels_list())
-
-
-# retrieve_results(1677375, 50)
+for result in results_list:
+    retrieve_results(result)
